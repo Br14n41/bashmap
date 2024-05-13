4f78e3daa80fab77f24de85761e7f2736788ebb4
Br14n41 <57382125+Br14n41@users.noreply.github.com>
2024-05-13 12:05:25 -0400

    Add files via upload

---
+++ bashmap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script to simplify nmap scanning
+
+
+
+# Check if filename is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <ip_file.txt>"
+    exit 1
+fi
+
+# Read the file line by line
+while IFS= read -r ip
+do
+    echo "Scanning IP: $ip"
+
+    # Run nmap to scan for open ports
+    ports=$(nmap -p- -T4 $ip | grep ^[0-9] | cut -d ' ' -f 1 | tr '\n' ',' | sed s/,$//)
+
+    # If ports were found, run service scan
+    if [ -n "$ports" ]; then
+        echo "Open ports: $ports"
+        echo "Running service scan..."
+        nmap -A -p$ports $ip
+    else
+        echo "No open ports found."
+    fi
+
+done < "$1"
