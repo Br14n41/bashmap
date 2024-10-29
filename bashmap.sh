95bdd04bd89286fbfb22e45a1b882d5f2b7efac3
Br14n41 <57382125+Br14n41@users.noreply.github.com>
2024-10-29 12:26:07 -0400

    Update bashmap.sh
    Added stats flag and made some small tweaks based on my own usage.

--- bashmap.sh
+++ bashmap.sh
@@ -2,7 +2,7 @@
 
 # Check if filename is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ip_file.txt>"
+    echo "Usage: bashmap <host_file>"
     exit 1
 fi
 
@@ -12,18 +12,18 @@ do
     echo "Scanning IP: $ip"
 
     # Run nmap to scan for open ports
-    ports=$(nmap -Pn -p- -T4 $ip | grep ^[0-9] | cut -d ' ' -f 1 | tr -d '/tcp' | paste -sd ',')
+    ports=$(nmap -Pn -p- $ip | grep ^[0-9] | cut -d ' ' -f 1 | tr -d '/tcp' | paste -sd ',')
 
     # If ports were found, run service scan
     if [ -n "$ports" ]; then
         echo "Open ports: $ports"
         echo "Running service scan..."
-        nmap -Pn -A -p$ports $ip -oX $ip.xml
+        nmap -sS -A -p$ports $ip --stats 10s -oA full_scan
     else
         echo "No open ports found."
     fi
 
 # Convert XML to HTML
-xsltproc $ip.xml -o $ip.html
+xsltproc full_scan.xml -o full_scan.html
 
 done < "$1"
